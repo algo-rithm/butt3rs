68ecb663459e1b786407f738ef78e6b453a0251a
Peter <balanesipf@gmail.com>
2019-03-08 19:06:30 -0500

    origin story

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pnut-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'butt3rs';
+}
